b5cb58e6d35aabf368f30f74276ff4c0dbfc95c1
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 14:01:17 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,6 +1,8 @@
 import pytest
-import src.exercise
+from src.exercise import print_numbers_in_range
 
-def test_exercise():
-    #implement tests here
-    assert 0 == 0
+def test_exercise(capsys):
+    numbers = [1,3,10,-1,5,0]
+    print_numbers_in_range(numbers,0,5)
+    out, err = capsys.readouterr()
+    assert out == "The numbers in the range [0, 5]\n1\n3\n5\n0\n", "Should read:\n 'The numbers in the range [0, 5]\n1\n3\n5\n0\n'"
